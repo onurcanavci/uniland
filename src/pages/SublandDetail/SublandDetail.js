39494a1307345e064e0ebd6faa19372df4537498
omeryilmaz <oyilmaz@rengen.io>
2022-04-27 18:09:26 +0300

    added subland functionalities

--- src/pages/SublandDetail/SublandDetail.js
+++ src/pages/SublandDetail/SublandDetail.js
@@ -1,13 +1,212 @@
-import { useLocation } from "react-router-dom";
+import {useLocation} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {customContract} from "../../contracts/contractUtils";
+import {UserContext} from "../../App";
+import {Container} from "@mui/material";
+
+function timeConverter(UNIX_timestamp) {
+    const a = new Date(UNIX_timestamp * 1000);
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const hour = a.getHours();
+    const min = a.getMinutes();
+    const sec = a.getSeconds();
+    return date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+}
 
 const SublandDetail = () => {
-  const history = useLocation();
+    const history = useLocation();
+    const {userAddress, contractWithSigner} = useContext(UserContext);
 
-  console.log("location: ", history.state);
+    const [isLoading, setIsLoading] = useState(false);
+    const [sublandInfo, setSublandInfo] = useState(null);
+    const [owner, setOwner] = useState(null)
+    const [name, setName] = useState("");
+    const [url, setUrl] = useState("");
+    const [authorizeField, setAuthorizeField] = useState({
+        address: null,
+        until_date: null,
+        until_time: null
+    });
 
-  
+    console.log("location: ", history.state);
 
-  return <p>sadasd</p>;
+    const isAuthorized = (address) => {
+        if (sublandInfo === null) {
+            return false
+        }
+        if (sublandInfo.authorized_address === '0x0000000000000000000000000000000000000000') {
+            return false
+        }
+        const currentTimestamp = Math.floor(Date.now() / 1000)
+        return address === sublandInfo.authorized_address && currentTimestamp <= sublandInfo.authorized_until
+    }
+
+    useEffect(() => {
+        console.log("userAddress: ", userAddress);
+        console.log("contract: ", contractWithSigner);
+
+        setIsLoading(true);
+        try {
+            customContract.sublands(history.state.mainLandId, history.state.sublandId).then((response) => {
+                setSublandInfo(response)
+            });
+            console.log('mainlandId', history.state.mainLandId)
+            customContract.ownerOf(history.state.mainLandId).then((ownerAddress) => {
+                setOwner(ownerAddress.toLowerCase())
+                console.log('owner', owner)
+                console.log('user', userAddress)
+                console.log(owner === userAddress)
+            })
+            console.log('sublandInfo', sublandInfo)
+        } catch (e) {
+            console.log("e: ", e);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [history.state.mainLandId, history.state.sublandId, isLoading]);
+
+    const transactionHandle = async (tx, message) => {
+        console.log('Tx sent')
+        try {
+            await tx.wait();
+            console.log('Mined')
+            setIsLoading(true)
+            alert(message)
+        } catch (e) {
+            alert(`An exception occurred ${e}`)
+        }
+    }
+    const handleNameChange = async (event) => {
+        event.preventDefault();
+        const tx = await contractWithSigner.updateSublandName(
+            history.state.mainLandId,
+            history.state.sublandId,
+            name
+        )
+        await transactionHandle(tx, 'Land Name Updated')
+    }
+    const handleUrlChange = async (event) => {
+        event.preventDefault();
+        const tx = await contractWithSigner.updateSublandURL(
+            history.state.mainLandId,
+            history.state.sublandId,
+            url
+        )
+        await transactionHandle(tx, 'Land Url Updated')
+    }
+
+    const handleAuthorize = async (event) => {
+        event.preventDefault();
+        console.log('authorizeField', authorizeField)
+        const dateString = authorizeField.until_date + 'T' + authorizeField.until_time;
+        const untilTimestamp = Math.floor(Date.parse(dateString) / 1000)
+        console.log('dateString', dateString)
+        console.log('parsed', untilTimestamp)
+        console.log()
+        const tx = await contractWithSigner.authorizeSubland(
+            history.state.mainLandId,
+            history.state.sublandId,
+            authorizeField.address,
+            untilTimestamp
+        )
+        await transactionHandle(tx, `Subland authorized to ${authorizeField.to} until ${timeConverter(untilTimestamp)}`)
+    }
+
+    if (isLoading) {
+        return <p>Loading</p>;
+    }
+
+    return (
+        <Container>
+            {
+                sublandInfo !== null && (
+                    <div>
+                        <p>Name: {sublandInfo.name}</p>
+                        <p>Url: {sublandInfo.url}</p>
+                        {
+                            isAuthorized(sublandInfo.authorized_address) && (
+                                <div>
+                                    <p>Authorized Address: {sublandInfo.authorized_address}</p>
+                                    <p>Authorized Until: {timeConverter(sublandInfo.authorized_until)}</p>
+                                </div>
+                            )
+                        }
+                    </div>
+                )
+            }
+            {
+                owner !== null && (
+                    <p>Owner: {owner}</p>
+                )
+            }
+            {
+                (userAddress === owner || isAuthorized(userAddress)) && (
+                    <div>
+                        <form onSubmit={handleNameChange}>
+                            <label>Change Subland Name:
+                                <input
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </label>
+                            <input type="submit"/>
+                        </form>
+                        <form onSubmit={handleUrlChange}>
+                            <label>Change Subland URL:
+                                <input
+                                    type="text"
+                                    value={url}
+                                    onChange={(e) => setUrl(e.target.value)}
+                                />
+                            </label>
+                            <input type="submit"/>
+                        </form>
+                    </div>
+                )
+            }
+            {
+                owner === userAddress && (
+                    <form onSubmit={handleAuthorize}>
+                        <label>Mint this land:
+                            <input
+                                type="text"
+                                placeholder="address"
+                                value={authorizeField.address}
+                                onChange={(e) => setAuthorizeField({
+                                    ...authorizeField,
+                                    address: e.target.value
+                                })}
+                            />
+                            <input
+                                type="date"
+                                placeholder="date"
+                                value={authorizeField.until_date}
+                                onChange={(e) => setAuthorizeField({
+                                    ...authorizeField,
+                                    until_date: e.target.value
+                                })}
+                            />
+                            <input
+                                type="time"
+                                placeholder="time"
+                                value={authorizeField.until_time}
+                                onChange={(e) => setAuthorizeField({
+                                    ...authorizeField,
+                                    until_time: e.target.value
+                                })}
+                            />
+                        </label>
+                        <input type="submit"/>
+                    </form>
+                )
+            }
+
+        </Container>
+    )
 };
 
 export default SublandDetail;
